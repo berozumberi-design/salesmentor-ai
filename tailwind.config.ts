1cb299914bc3342060ca3b4e654c9e3c92122a4d
berozumberi-design <bero.zumberi@gmail.com>
2026-03-06 22:18:46 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
-};
-export default config;
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
